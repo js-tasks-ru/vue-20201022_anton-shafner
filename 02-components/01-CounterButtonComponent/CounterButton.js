858315306d5c700ba5eab6a8a0f0e4d7d643e07e
dragooon4ik24 <shafner.anton@gmail.com>
2020-11-01 16:09:03 +0300

    added solution for 01-CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,6 +1,28 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: `<button type="button" @click="handleClick">{{localCount}}</button>`,
+  props: {
+    count: Number,
+    required: true,
+    default: 0,
+  },
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+  computed: {
+    localCount() {
+      if (!!this.count) {
+        return this.count;
+      }
+      return 0;
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('increment', this.localCount + 1)
+    }
+  }
 
   // Компонент должен иметь входной параметр
 
