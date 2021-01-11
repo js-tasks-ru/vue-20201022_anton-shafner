fa2f4c167257dd0a2d2b9b8597a67fb760b75580
dragooon4ik24 <shafner.anton@gmail.com>
2021-01-11 19:36:02 +0300

    added solution for 04-ScrollBehavior

--- 04-SPA/02-ScrollBehavior/router/index.js
+++ 04-SPA/02-ScrollBehavior/router/index.js
@@ -3,8 +3,23 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-export function scrollBehavior() {
-  // Место для решения
+export function scrollBehavior(to, from, savedPosition) {
+  if (savedPosition) {
+    return savedPosition;
+  }
+  if (to.hash) {
+    return {
+      selector: to.hash,
+    };
+  }
+  const getEnableSaveScrollMeta = (route) => {
+    return route.matched.some((record) => record.meta.saveScrollPosition);
+  };
+  if (getEnableSaveScrollMeta(from) && getEnableSaveScrollMeta(to)) {
+    return false;
+  }
+
+  return { x: 0, y: 0 };
 }
 
 export const router = new VueRouter({
