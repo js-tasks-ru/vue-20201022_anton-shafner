a24b06395e6a379bb72f9974f34176d8b3e938f6
dragooon4ik24 <shafner.anton@gmail.com>
2021-05-13 17:22:39 +0300

    07-form/01-CounterButton_last_final

--- 07-forms/01-CounterButton_last_final/components/CounterButton.vue
+++ 07-forms/01-CounterButton_last_final/components/CounterButton.vue
@@ -1,9 +1,34 @@
 <template>
-  <button></button>
+  <button @click="$emit('increment', ++count_)">{{ count_ }}</button>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'CounterButton',
+  model: {
+    prop: 'count',
+    event: 'increment'
+  },
+  data() {
+    return {
+      count_: 0
+    }
+  },
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
+  watch: {
+    count: {
+      immediate: true,
+      handler(val) {
+        this.count_ = val;
+      }
+    }
+  }
+};
 </script>
 
 <style>
